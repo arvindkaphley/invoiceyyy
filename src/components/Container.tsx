a8504e9283cbb3528a7adbc84b57178abbdce570
arvindkaphley <kaphleyarvind19@gmail.com>
2024-11-26 23:35:51 +0530

    Minor Changes

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,13 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-interface CointainerProps extends React.ComponentProps<"div">{
+interface ContainerProps extends React.ComponentProps<"div"> {}
 
-}
-const Container=({children,className,...props}: CointainerProps)=>{
-    return (
-        <div {...props} className={cn('max-w-5xl mx-auto px-5',className)}>
-            {children}
-        </div>
-    )
-}
+const Container = ({ children, className, ...props }: ContainerProps) => {
+  return (
+    <div {...props} className={cn("max-w-5xl mx-auto px-5", className)}>
+      {children}
+    </div>
+  );
+};
 
 export default Container;
